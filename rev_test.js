e161d26b52c20af13550b9dcff29a2b291324d1e
April Xie <aprilxie@mit.edu>
2019-09-14 14:19:57 -0400

    prints fillers uh and um

--- rev_test.js
+++ rev_test.js
@@ -35,4 +35,20 @@ const token = "02BxYhsK1vX4kf7yVf2vqWWIxk3dyFq6zvK4PMAgWGnmSotRvBY-8x7bRigOeaQ6r
      */
     var transcriptText = await client.getTranscriptText(job.id);
     console.log(transcriptText);
+
+    var sample = "Hi, this new recording. Um, I got, I got a filler word in there. Uh, another one, the C, I'm trying to avoid the killer words."
+
+    function getFillerWords(text) {
+        var textWords = text.split(/[^a-zA-Z']/).filter(function(word){return word !== ''});
+        var fillers = textWords.filter(isFiller);
+        console.log(fillers);
+    }
+    
+    const fillers = ["uh", "um"];
+
+    function isFiller(word) {
+        return fillers.includes(word.toLowerCase());
+    }
+
+    getFillerWords(sample);
 })();
