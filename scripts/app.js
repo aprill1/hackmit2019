44241d4c7a113b20d2adaf16c6ceae5c2e717b2c
YaseenAlk <alkhafaji.yaseen@gmail.com>
2019-09-14 16:50:44 -0400

    started converting rev node to rev api/js

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,3 @@
-import { uploadToAPI } from 'https://raw.githubusercontent.com/aprill1/hackmit2019/master/scripts/rev_node.js';
 /**
  * Contributors: Yaseen Alkhafaji, April Xie, Brian Hoh, Carlos Trevino
  * 
@@ -106,7 +105,7 @@ if (navigator.mediaDevices.getUserMedia) {
         }
       }
 
-      uploadToAPI(audio.src);
+      uploadToAPI(blob);
     }
 
     mediaRecorder.ondataavailable = function(e) {
@@ -182,3 +181,97 @@ window.onresize = function() {
 }
 
 window.onresize();
+
+// API CODE BELOW
+async function uploadToAPI(URL)
+{
+    async function getTranscript(id)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", "https://api.rev.ai/speechtotext/v1/jobs/" + id + "/transcript", true); // true for asynchronous
+        xmlHttp.setRequestHeader("Authorization", "Bearer 02BxYhsK1vX4kf7yVf2vqWWIxk3dyFq6zvK4PMAgWGnmSotRvBY-8x7bRigOeaQ6rg9Hf8aNpLUUmj3bo-hkkFZwpyefo");
+        xmlHttp.setRequestHeader("Accept", "text/plain");
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                console.log(xmlHttp.responseText);
+                return xmlHttp.responseText;
+            }
+        } 
+        xmlHttp.send(null);
+    }
+
+    async function checkStatus(id)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", "https://api.rev.ai/speechtotext/v1/jobs/" + id, true); // true for asynchronous 
+        xmlHttp.setRequestHeader("Authorization", "Bearer 02BxYhsK1vX4kf7yVf2vqWWIxk3dyFq6zvK4PMAgWGnmSotRvBY-8x7bRigOeaQ6rg9Hf8aNpLUUmj3bo-hkkFZwpyefo");
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                console.log(xmlHttp.responseText);
+                return xmlHttp.responseText;
+            }
+        }
+        xmlHttp.send(null);
+    }
+
+    async function sendFileAsync(url)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("POST", "https://api.rev.ai/speechtotext/v1/jobs", true); // true for asynchronous 
+        xmlHttp.setRequestHeader("Authorization", "Bearer 02BxYhsK1vX4kf7yVf2vqWWIxk3dyFq6zvK4PMAgWGnmSotRvBY-8x7bRigOeaQ6rg9Hf8aNpLUUmj3bo-hkkFZwpyefo");
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                console.log(xmlHttp.responseText);
+                return xmlHttp.responseText;
+            }
+        }
+        var asForm = new FormData();
+
+        asForm.append("media", url);// new File(url, "myfile.ogg"));
+        asForm.append("type", "audio/ogg");
+        xmlHttp.send(asForm);
+    }
+
+  console.log("---------- uploading to API ----------");
+  // Initialize your client with your revai access token
+  //var client = new revai.RevAiApiClient(token);
+
+  // Get account details
+  //var account = await client.getAccount();
+  //console.log(`Account: ${account.email}`);
+  //console.log(`Balance: ${account.balance_seconds} seconds`);
+
+  // Media may be submitted from a local file
+  //var job = await client.submitJobUrl(URL);
+  var res = await sendFileAsync(URL);
+  console.log(res);
+  var job = JSON.parse(res);
+
+  console.log(`Job Id: ${job.id}`);
+  console.log(`Status: ${job.status}`);
+  console.log(`Created On: ${job.created_on}`);
+
+  /**
+   * Waits 5 seconds between each status check to see if job is complete.
+   * note: polling for job status is not recommended in a non-testing environment.
+   * Use the callback_url option (see: https://www.rev.ai/docs#section/Node-SDK)
+   * to receive the response asynchronously on job completion
+   */
+
+  while((jobStatus = (await JSON.parse(checkStatus(job.id)).status)) == "in_progress")
+  {  
+      console.log(`Job ${job.id} is ${jobStatus}`);
+      await new Promise( resolve => setTimeout(resolve, 5000));
+  }
+
+  /** 
+   * Get transcript as plain text
+   */
+  //var transcriptText = await client.getTranscriptText(job.id);
+  var transcriptText = getTranscript(id);
+  console.log(transcriptText);
+  console.log("---------- done uploading to API ----------");
+}
--- scripts/rev_node.js
+++ scripts/rev_node.js
@@ -4,17 +4,66 @@ const token = "02BxYhsK1vX4kf7yVf2vqWWIxk3dyFq6zvK4PMAgWGnmSotRvBY-8x7bRigOeaQ6r
 
 export async function uploadToAPI(URL)
 {
+    function getTranscript(id)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.setRequestHeader("Authorization", "Bearer 02BxYhsK1vX4kf7yVf2vqWWIxk3dyFq6zvK4PMAgWGnmSotRvBY-8x7bRigOeaQ6rg9Hf8aNpLUUmj3bo-hkkFZwpyefo");
+        xmlHttp.setRequestHeader("Accept", "text/plain");
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                console.log(xmlHttp.responseText);
+                return xmlHttp.responseText;
+            }
+        }
+        xmlHttp.open("GET", "https://api.rev.ai/speechtotext/v1/jobs/" + id + "/transcript", true); // true for asynchronous 
+        xmlHttp.send(null);
+    }
+
+    function checkStatus(id)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.setRequestHeader("Authorization", "Bearer 02BxYhsK1vX4kf7yVf2vqWWIxk3dyFq6zvK4PMAgWGnmSotRvBY-8x7bRigOeaQ6rg9Hf8aNpLUUmj3bo-hkkFZwpyefo");
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                console.log(xmlHttp.responseText);
+                return xmlHttp.responseText;
+            }
+        }
+        xmlHttp.open("GET", "https://api.rev.ai/speechtotext/v1/jobs/" + id, true); // true for asynchronous 
+        xmlHttp.send(null);
+    }
+
+    function sendFileAsync(url)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.setRequestHeader("Authorization", "Bearer 02BxYhsK1vX4kf7yVf2vqWWIxk3dyFq6zvK4PMAgWGnmSotRvBY-8x7bRigOeaQ6rg9Hf8aNpLUUmj3bo-hkkFZwpyefo");
+        xmlHttp.setRequestHeader("Content-Type", "application/json");
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                console.log(xmlHttp.responseText);
+                return xmlHttp.responseText;
+            }
+                
+        }
+        xmlHttp.open("POST", "https://api.rev.ai/speechtotext/v1/jobs", true); // true for asynchronous 
+        xmlHttp.send("{\"media_url\":\"" + url + "\",\"metadata\":\"A voice recording we want to test\"}");
+    }
+
   console.log("---------- uploading to API ----------");
   // Initialize your client with your revai access token
-  var client = new revai.RevAiApiClient(token);
+  //var client = new revai.RevAiApiClient(token);
 
   // Get account details
-  var account = await client.getAccount();
-  console.log(`Account: ${account.email}`);
-  console.log(`Balance: ${account.balance_seconds} seconds`);
+  //var account = await client.getAccount();
+  //console.log(`Account: ${account.email}`);
+  //console.log(`Balance: ${account.balance_seconds} seconds`);
 
   // Media may be submitted from a local file
-  var job = await client.submitJobUrl(URL);
+  //var job = await client.submitJobUrl(URL);
+  var job = await JSON.parse(sendFileAsync(URL));
 
   console.log(`Job Id: ${job.id}`);
   console.log(`Status: ${job.status}`);
@@ -26,7 +75,8 @@ export async function uploadToAPI(URL)
    * Use the callback_url option (see: https://www.rev.ai/docs#section/Node-SDK)
    * to receive the response asynchronously on job completion
    */
-  while((jobStatus = (await client.getJobDetails(job.id)).status) == "in_progress")
+
+  while((jobStatus = (await JSON.parse(checkStatus(job.id)).status)) == "in_progress")
   {  
       console.log(`Job ${job.id} is ${jobStatus}`);
       await new Promise( resolve => setTimeout(resolve, 5000));
@@ -35,7 +85,8 @@ export async function uploadToAPI(URL)
   /** 
    * Get transcript as plain text
    */
-  var transcriptText = await client.getTranscriptText(job.id);
+  //var transcriptText = await client.getTranscriptText(job.id);
+  var transcriptText = 
   console.log(transcriptText);
   console.log("---------- done uploading to API ----------");
 }
